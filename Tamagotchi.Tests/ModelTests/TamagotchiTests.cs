84696c97ca43ee881d17e3086b79e940a04d492c
noahkise <noah@kisefamily.com>
2024-01-24 10:22:19 -0800

    pass id test
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
+++ Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
@@ -6,17 +6,17 @@ using System;
 namespace Tamagotchi.Tests
 {
   [TestClass]
-  public class PetTests
+  public class PetTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Pet.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Pet.ClearAll();
+    }
 
     [TestMethod]
     public void PetOpeningConstructor_CreatesInstanceOfPet_Pet()
     {
-      Pet newPet = new("brad", "yellow", 2, 2, 2);
+      Pet newPet = new("brad", "yellow", 2, 2, 2, true);
       Assert.AreEqual(typeof(Pet), newPet.GetType());
     }
 
@@ -28,12 +28,14 @@ namespace Tamagotchi.Tests
       int happiness = 2;
       int hunger = 2;
       int energy = 2;
-      Pet brad = new(name, color, happiness, hunger, energy);
+      bool isAlive = true;
+      Pet brad = new(name, color, happiness, hunger, energy, isAlive);
       Assert.AreEqual(brad.Name, name);
       Assert.AreEqual(brad.Color, color);
       Assert.AreEqual(brad.Happiness, happiness);
       Assert.AreEqual(brad.Hunger, hunger);
       Assert.AreEqual(brad.Energy, energy);
+      Assert.AreEqual(brad.IsAlive, isAlive);
     }
 
     [TestMethod]
@@ -44,17 +46,34 @@ namespace Tamagotchi.Tests
       int happiness = 2;
       int hunger = 2;
       int energy = 2;
-      Pet brad = new(name, color, happiness, hunger, energy);
+      bool isAlive = true;
+      Pet brad = new(name, color, happiness, hunger, energy, isAlive);
       brad.Name = name;
       brad.Color = color;
       brad.Happiness = happiness;
       brad.Hunger = hunger;
       brad.Energy = energy;
+      brad.IsAlive = isAlive;
       Assert.AreEqual(brad.Name, name);
       Assert.AreEqual(brad.Color, color);
       Assert.AreEqual(brad.Happiness, happiness);
       Assert.AreEqual(brad.Hunger, hunger);
       Assert.AreEqual(brad.Energy, energy);
+      Assert.AreEqual(brad.IsAlive, isAlive);
+    }
+
+    [TestMethod]
+    public void GetId_PetInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string name = "brad";
+      string color = "yellow";
+      int happiness = 2;
+      int hunger = 2;
+      int energy = 2;
+      bool isAlive = true;
+      Pet brad = new(name, color, happiness, hunger, energy, isAlive);
+      int result = brad.Id;
+      Assert.AreEqual(1, result);
     }
 
   }
--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -10,21 +10,56 @@ namespace Tamagotchi.Models
         public int Happiness { get; set; }
         public int Hunger { get; set; }
         public int Energy { get; set; }
+
+        public bool IsAlive { get; set; }
+
+        public int Id { get; }
         private static List<Pet> _instances = new() { };
 
-        public Pet(string name, string color, int happiness, int hunger, int energy)
+        public Pet(string name, string color, int happiness, int hunger, int energy, bool isAlive)
         {
             Name = name;
             Color = color;
             Happiness = happiness;
             Hunger = hunger;
             Energy = energy;
+            IsAlive = isAlive;
             _instances.Add(this);
+            Id = _instances.Count;
         }
 
+        public void Feed()
+        {
+            this.Hunger -= 1;
+        }
+
+        public void Attention()
+        {
+            this.Happiness += 1;
+        }
+
+        public void Rest()
+        {
+            this.Energy += 1;
+        }
+
+        public void Die()
+        {
+            this.IsAlive = false;
+        }
+
+        public static List<Pet> GetAll()
+        {
+            return _instances;
+        }
         public static void ClearAll()
         {
             _instances.Clear();
+        }
+
+        public static Pet Find(int searchID)
+        {
+            return _instances[searchID-1];
         }
 
     }
