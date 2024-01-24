c8b1ce6f2c3c3341443f5b41cfc1a3919e099768
noahkise <noah@kisefamily.com>
2024-01-24 10:49:47 -0800

    details successfully displaying based on id
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- Tamagotchi/Controllers/TamagotchisController.cs
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -1,32 +1,39 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Tamagotchi.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+using System.Collections.Generic;
 
-// namespace Tamagotchi.Controllers
-// {
-//   public class TamagotchisController : Controller
-//   {
+namespace Tamagotchi.Controllers
+{
+  public class TamagotchisController : Controller
+  {
 
-//     [HttpGet("/tamagotchis")]
-//     public ActionResult Index()
-//     {
+    [HttpGet("/pets")]
+    public ActionResult Index()
+    {
 
-//       List<Tamagotchi> allTamagotchis = Tamagotchi.GetAll();
-//       return View(allTamagotchis);
-//     }
+      List<Pet> allTamagotchis = Pet.GetAll();
+      return View(allTamagotchis);
+    }
 
-//     [HttpGet("/tamagotchis/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
+    [HttpGet("/pets/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
 
-//     [HttpPost("/tamagotchis")]
-//     public ActionResult Create(string name, string color, int happiness, int hunger, int energy)
-//     {
-//       Tamagotchi myTamagotchi = new Tamagotchi(name, color, happiness, hunger, energy);
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost("/pets")]
+    public ActionResult Create(string name, string color)
+    {
+      Pet myTamagotchi = new Pet(name, color, 10, 0, 10, true);
+      return RedirectToAction("Index");
+    }
 
-//   }
-// }
+     [HttpGet("/pets/{id}")]
+    public ActionResult Details(int id)
+    {
+      Pet foundPet = Pet.Find(id);
+      return View(foundPet);
+    }
+
+  }
+}
