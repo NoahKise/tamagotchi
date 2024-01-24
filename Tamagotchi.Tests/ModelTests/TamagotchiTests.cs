ed1565196f33859acce01c18456a020ab873064d
noahkise <noah@kisefamily.com>
2024-01-24 09:55:22 -0800

    pass set test
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
+++ Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
@@ -34,7 +34,27 @@ namespace Tamagotchi.Tests
       Assert.AreEqual(brad.Happiness, happiness);
       Assert.AreEqual(brad.Hunger, hunger);
       Assert.AreEqual(brad.Energy, energy);
+    }
 
+    [TestMethod]
+    public void PetConstructor_SetProperties()
+    {
+      string name = "brad";
+      string color = "yellow";
+      int happiness = 2;
+      int hunger = 2;
+      int energy = 2;
+      Pet brad = new(name, color, happiness, hunger, energy);
+      brad.Name = name;
+      brad.Color = color;
+      brad.Happiness = happiness;
+      brad.Hunger = hunger;
+      brad.Energy = energy;
+      Assert.AreEqual(brad.Name, name);
+      Assert.AreEqual(brad.Color, color);
+      Assert.AreEqual(brad.Happiness, happiness);
+      Assert.AreEqual(brad.Hunger, hunger);
+      Assert.AreEqual(brad.Energy, energy);
     }
 
   }
